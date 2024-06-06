8530e7e2588158bf4934900db6645ac23e823571
진냥이 <106366798+hjeane@users.noreply.github.com>
2024-06-06 13:36:08 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -24,7 +24,8 @@ def main():
     page_title="DirChat",
     page_icon=":books:")
 
-    st.title("_Private Data :red[QA Chat]_ :books:")
+    st.title("문정에게 물어봐:red[QA Chat]_ :books:")
+    st.caption(":pencil2: A Library and Information Science chatbot powered by OpenAI")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -53,7 +54,7 @@ def main():
 
     if 'messages' not in st.session_state:
         st.session_state['messages'] = [{"role": "assistant", 
-                                        "content": "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
+                                        "content": ":speech_balloon: 안녕하세요! 저의 이름은 AI 사서 챗봇 LISBOT이에요! 문헌정보학에 관해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -62,7 +63,7 @@ def main():
     history = StreamlitChatMessageHistory(key="chat_messages")
 
     # Chat logic
-    if query := st.chat_input("질문을 입력해주세요."):
+    if query := st.chat_input("무엇을 도와드릴까요? 질문을 입력해주세요!"):
         st.session_state.messages.append({"role": "user", "content": query})
 
         with st.chat_message("user"):

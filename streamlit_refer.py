1aa2334496faac31604657be2e407acf032752fa
진냥이 <106366798+hjeane@users.noreply.github.com>
2024-06-12 11:50:43 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -25,7 +25,6 @@ def main():
     page_icon=":books:")
 
     st.title(" :red[:books: LISTBOT] 에게 물어보세요	:grey_exclamation:")
-    st.caption("    :closed_book: Hello, welcome to the Library and Information Science Q&A chat. ")
     st.caption("    :closed_book: This app is developed for the Introduction to Data Science course project for Spring 2024 at Chung-ang University. Feel free to ask any questions to LISBOT. Whether you're looking for research help, resource recommendations, or answers to specific questions, LISBOT is here to assist you.")
     
     if "conversation" not in st.session_state:
